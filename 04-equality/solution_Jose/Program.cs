5d0dedb0477136cc9e58b2cb7b0ed5efba9e3cb8
jose-guerrero <jose.guerrerog@outlook.com>
2018-03-20 10:05:22 +1100

    little shorter

--- 04-equality/solution_Jose/Program.cs
+++ 04-equality/solution_Jose/Program.cs
@@ -5,10 +5,11 @@ namespace solution_Jose
     class Program
     {
         static int cont = 1;
-        public static int a => cont++;
+        static int a => cont++; 
         static void Main(string[] args)
         {
             Console.WriteLine(a==1 && a==2&& a==3);
         }
     }
 }
+ 
