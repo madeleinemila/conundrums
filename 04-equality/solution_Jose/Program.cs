202bb598b6e21def7a257c8d03fae2cdf7ac55ba
jose-guerrero <jose.guerrerog@outlook.com>
2018-03-20 09:33:39 +1100

    solution Jose

---
+++ 04-equality/solution_Jose/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace solution_Jose
+{
+    class Program
+    {
+        static int cont = 1;
+        public static int a => cont++;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(a==1 && a==2&& a==3);
+        }
+    }
+}
